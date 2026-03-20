bugfix: wrong indexing in normalization code

src/DE_analysis_optimizer/pipeline_steps.py
@@ -92,7 +92,7 @@ class MeanCenter(Step):
         vals = data.get_data()
         means = np.nanmean(vals, axis = 0)
         grand_mean = np.nanmean(vals)
-        vals = (vals/means[:, np.newaxis])*grand_mean
+        vals = (vals/means[np.newaxis, :])*grand_mean
         data.set_data(vals)
         return data
 
@@ -106,7 +106,7 @@ class LogMeanCenter(Step):
         vals = np.log(vals)
         means = np.nanmean(vals, axis = 0)
         grand_mean = np.nanmean(vals)
-        vals = (vals/means[:, np.newaxis])*grand_mean
+        vals = (vals/means[np.newaxis, :])*grand_mean
         vals = np.exp(vals)
         data.set_data(vals)
         return data
@@ -120,7 +120,7 @@ class MedianCenter(Step):
         vals = data.get_data()
         medians = np.nanmean(vals, axis = 0)
         grand_median = np.nanmedian(vals)
-        vals = (vals/medians[:, np.newaxis])*grand_median
+        vals = (vals/medians[np.newaxis, :])*grand_median
         data.set_data(vals)
         return data
 
@@ -134,7 +134,7 @@ class LogMedianCenter(Step):
         vals = np.log(vals)
         medians = np.nanmean(vals, axis = 0)
         grand_median = np.nanmedian(vals)
-        vals = (vals/medians[:, np.newaxis])*grand_median
+        vals = (vals/medians[np.newaxis, :])*grand_median
         vals = np.exp(vals)
         data.set_data(vals)
         return data

tests/pipeline_steps_tests.py
@@ -24,13 +24,13 @@ class NormalizationTestSuite(testSuite_ancestor_objs.baseLipidomicsTestSuite):
         A = self.data.get_A()
         A *= 1.2
         self.data.set_A(A)
-        init_mean_diff = np.mean(self.data.get_A()) - np.mean(self.data.get_B())
+        init_mean_diff = np.nanmean(self.data.get_A()) - np.nanmean(self.data.get_B())
         
         for step_option in self.step_options:
             if step_option not in ['noop']:
                 data = deepcopy(self.data)
                 data = self.pipeline_steps[step_option].process(data)
-                mean_diff = np.mean(data.get_A()) - np.mean(data.get_B())
+                mean_diff = np.nanmean(data.get_A()) - np.nanmean(data.get_B())
             
                 with self.subTest(f'Can {step_option} remove a multiplicative shift?'):
                     self.assertLess(mean_diff, init_mean_diff)