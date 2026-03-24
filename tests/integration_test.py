04ec093bab409f6fa932be00bb7ee22e53134107
stavis1 <steventavis@gmail.com>
2026-03-24 16:11:56 +0100

    refactored integration test to be more representative of the whole pipeline

---
+++ tests/integration_test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 12:18:53 2026
+
+@author: chemod-u02
+"""
+import unittest
+from multiprocessing import Pool, set_start_method
+import time
+
+import testSuite_ancestor_objs
+from DE_analysis_optimizer.workers import run_optimization_worker
+from DE_analysis_optimizer.utils import read_data, init_data_manager
+
+class workersTestSuite(testSuite_ancestor_objs.baseProteomicsTestSuite):
+    def test_integration(self):
+        initial_data = read_data(self.options)
+        
+        with Pool(self.options.cores) as p:
+            #initialize the manager process for attempts and outcomes data
+            pipes = init_data_manager(self.options, p)
+            
+            #construct jobs
+            n_workers = len(pipes)
+            jobs = zip([self.options]*n_workers, [initial_data]*n_workers, pipes)
+            
+            #run parallel worker loops
+            p.starmap_async(run_optimization_worker, jobs)
+            time.sleep(2)
+            p.terminate()
+
+        #assess results
+        with self.subTest('Were outcome messages saved to file?'):
+            with open('test_output/outcomes.tsv', 'r') as tsv:
+                text = tsv.readlines()
+                self.assertGreater(len(text), 1)
+
+if __name__ == '__main__':
+    import make_test_data
+    set_start_method('fork')
+    unittest.main()
+        
+        
+        
+        
--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -13,7 +13,7 @@ from utils_tests import *
 from data_tests import *
 from pipeline_tests import *
 from genetic_algorithm_tests import *
-from workers_tests import *
+from integration_test import *
 from pipeline_steps_tests import *
 
 if __name__ == '__main__':
